9e6b68e494d7c74dc85c9da834b1e9cc7657a820
Daniel Saldinata <dansaldt@protonmail.com>
2021-08-28 19:09:43 +0700

    remote temporary test code

--- src/main.cpp
+++ src/main.cpp
@@ -8,51 +8,6 @@
 
 namespace fs = std::filesystem;
 
-void demo_exists (
-	  const fs::path &p,
-	  fs::file_status s = fs::file_status{} )
-{
-	std::cout << p;
-	if (fs::status_known(s) ? fs::exists(s) : fs::exists(p)) {
-		std::cout << " exits:\n";
-	} else {
-		std::cout << " does not exists\n";
-	}
-}
-
-void test_filepath ()
-{
-	const fs::path target_path = {R"(C:\eWAM_6_1_5_32\eWAM\god)"};
-
-	if (!fs::exists(target_path)) {
-		std::cout << "target_path: \"" << target_path << "\" does not exist\n";
-//				return -1;
-	}
-
-	std::cout << "target_path: " << target_path << '\n';
-	std::cout << "root_name: " << target_path.root_name() << '\n';
-	std::cout << "root_directory: " << target_path.root_directory() << '\n';
-	std::cout << "root_path: " << target_path.root_path() << '\n';
-	std::cout << "relative_path: " << target_path.relative_path() << '\n';
-	std::cout << "parent_path: " << target_path.parent_path() << '\n';
-	std::cout << "filename: " << target_path.filename() << '\n';
-	std::cout << "stem: " << target_path.stem() << '\n';
-	std::cout << "extension: " << target_path.extension() << '\n';
-
-	for (auto &curr: fs::recursive_directory_iterator(target_path)) {
-		if (curr.is_directory()) {
-			std::cout << "dir: " << curr.path() << '\n';
-		} else if (curr.is_regular_file()) {
-//						std::cout << "file: ";
-		} else {
-//						std::cout << "other: ";
-		}
-//				std::cout << curr.path() << '\n';
-	}
-
-//		return 0;
-}
-
 const auto env_var_wyde_root = "WYDE-ROOT";
 const auto bundle_index_filename = "bundle-index.txt";
 
@@ -165,7 +120,8 @@ int main ()
 			cur_class_module = buf;
 			trim(cur_class_module);
 			if (cur_bundle1.empty() || cur_bundle2.empty()) {
-				std::cout << "ERROR: cannot put class/module \"" << cur_class_module << " to any bundle: "
+				std::cout << "ERROR: cannot put class/module \"" << cur_class_module
+					  << " to any bundle: "
 					  << "cur_bundle1=\"" << cur_bundle1 << "\", "
 					  << "cur_bundle2=\"" << cur_bundle2 << "\".\n";
 			} else {

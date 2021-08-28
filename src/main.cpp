470a0e6b4c61f5fbe90abe0c578f04232552a733
Daniel Saldinata <dansaldt@protonmail.com>
2021-08-28 19:07:23 +0700

    add ltrim, rtrim, and trim

--- src/main.cpp
+++ src/main.cpp
@@ -63,6 +63,30 @@ typedef struct wydecontext {
     std::map<std::string, fs::path> bundles;
 } wydecontext;
 
+// trim from start (in place)
+static inline void ltrim ( std::string &s )
+{
+	s.erase(s.begin(), std::find_if(s.begin(), s.end(), [] ( unsigned char ch ) {
+	    return !std::isspace(ch);
+	}));
+}
+
+// trim from end (in place)
+static inline void rtrim ( std::string &s )
+{
+	s.erase(std::find_if(s.rbegin(), s.rend(), [] ( unsigned char ch ) {
+	    return !std::isspace(ch);
+	}).base(), s.end());
+}
+
+// trim from both ends (in place)
+static inline void trim ( std::string &s )
+{
+	ltrim(s);
+	rtrim(s);
+}
+
+
 int init ( wydecontext &wp )
 {
 	// get wyde-root environment variable
